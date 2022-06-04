3621e8d2a5a6d6ca5033586b2a3033245271768d
anjali <anjalivp92@gmail.com>
2022-06-04 17:58:05 +0530

    first commit

---
+++ public/js/nav.js
@@ -0,0 +1,18 @@
+const nav= [         // part#2  point 6
+    {
+        link:"/books",
+        title:"Books"
+    },
+    {
+        link:"/authors",
+        title:"Authors"
+    },
+    {
+        link:"/addbook",
+        title:"Add Book"
+    },
+    {
+        link:"/addauthor",
+        title:"Add Author"
+    }
+]
